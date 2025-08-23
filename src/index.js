36bb132c966f030546832e8f001f4d366933ba20
Mahmoud-Ahmed200 <ma2582005@gmail.com>
2025-08-23 22:10:31 +0300

    Setup base project structure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT;
+app.use(express.json());
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
